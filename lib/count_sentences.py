983cea38866ea408a75449073ebcdaaed7b49c42
Samuel Maiko <sammymaiko721@gmail.com>
2023-08-24 08:37:37 +0300

    python code attempt

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self):
+    self._word="Hey"
+
+  @property
+  def value(self):
+    return self._word
+  
+  @value.setter
+  def value(self,new_value):
+    if type(new_value) is str:
+      self._word=new_value
+
+    else:
+      print("The value must be a string.")
+  def is_sentence(self):
+    return self._word.endswith(".")
+  def is_question(self):
+    return self._word.endswith("?")
+  def is_exclamation(self):
+    return self._word.endswith("?")
+  def count_sentences(self):
+    modified_version=self.word.replace("!","*").replace(".","*").replace("?","*")
+    the_list=modified_version.split("* ")
+    return len(the_list)
+    
+# string=MyString()
+# string.value="Hallo"
+# print(string.value)
